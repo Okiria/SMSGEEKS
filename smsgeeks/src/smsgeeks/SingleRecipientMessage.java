86d0afc64a0c511b77bf3ea0b1e17b7335e641bc
emmaokiria <emmaokiria@gmail.com>
2012-10-29 18:02:45 +0300

    small edits

--- smsgeeks/src/smsgeeks/SingleRecipientMessage.java
+++ smsgeeks/src/smsgeeks/SingleRecipientMessage.java
@@ -12,6 +12,7 @@ public class SingleRecipientMessage extends OutBoundMessage {
     
     private ContactNumber RecieverNumber;
     
+    
     private SingleRecipientMessage(MessageDirection MessageDirection, MessageType MessageType, String SenderAlias, SMSSendReport ReportOnSend, long TimeSent, long Validity, int NumberOfSentMessages, MessageStatus MessageStatus, String SendEventName, ContactNumber RecieverNumber){
         super(MessageDirection, MessageType, SenderAlias, ReportOnSend, TimeSent, Validity, NumberOfSentMessages, MessageStatus, SendEventName);
         
